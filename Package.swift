d4e13e474ffa890d0e4153a9840662a7dd438318
Kieran Brown <kb6627500@gmail.com>
2020-08-31 09:42:33 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "EventGen",
+    products: [
+        .executable(name: "EventGen", targets: ["EventGen"]),
+    ],
     dependencies: [
 
        .package(name: "swift-argument-parser",
